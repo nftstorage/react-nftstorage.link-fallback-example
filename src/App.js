9e99e8635c6285f473c85d64f50cb312f59273bf
Alan Shaw <alan.shaw@protocol.ai>
2022-04-27 12:11:53 +0100

    feat: wip add nftstorage.link lib

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { getGatewayURL, GatewayStatusChecker } from 'nftstorage.link'
+
+const fakeStatusChecker = {
+  ok: () => new Promise(resolve => resolve('NOT OK')),
+  gatewayURL: 'https://nftstorage.link'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+function GatewayLink({ cid, title }) {
+  const [url, setUrl] = useState('')
+  useEffect(async () => setUrl(await getGatewayURL(cid)), [cid])
+  return url ? <a href={url}>{title}</a> : null
+}
+
+function GatewayImage({ cid, alt }) {
+  const [url, setUrl] = useState('')
+  useEffect(async () => setUrl(await getGatewayURL(cid)), [cid])
+  return url ? <img src={url} alt={alt} /> : null
+}
+
+export default App

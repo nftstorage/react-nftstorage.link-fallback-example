2e09e06464cb4ad6c0175854abc3933b6aa6857d
Alan Shaw <alan.shaw@protocol.ai>
2022-04-27 15:38:57 +0100

    feat: make work

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
-import { getGatewayURL, GatewayStatusChecker } from 'nftstorage.link'
+import { useState, useEffect } from 'react'
+import { getGatewayURL } from 'nftstorage.link'
 
-const fakeStatusChecker = {
-  ok: () => new Promise(resolve => resolve('NOT OK')),
-  gatewayURL: 'https://nftstorage.link'
-}
+export default function App () {
+  const [cid, setCid] = useState('bafkreiem4twkqzsq2aj4shbycd4yvoj2cx72vezicletlhi7dijjciqpui')
+  const [simulateBadStatus, setSimulateBadStatus] = useState(false)
+  const statusChecker = simulateBadStatus ? fakeStatusChecker : undefined
 
-function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ma4'>
+      <h1>nftstorage.link fallback</h1>
+      <label className='db mb3'>
+        CID:
+        <input className='input-reset courier ba pa2 ml1' value={cid} onInput={e => setCid(e.target.value)} style={{ width: '38rem' }} />
+      </label>
+      <p className='f6'>Try <span className='courier'>bafkreiajkbmpugz75eg2tmocmp3e33sg5kuyq2amzngslahgn6ltmqxxfa</span></p>
+      <label className='db mb3'>
+        <input className='mr1' type='checkbox' value={simulateBadStatus} onChange={e => setSimulateBadStatus(e.target.checked)} />
+        Simulate bad status
+      </label>
+      <hr className='ba mv3' />
+      <h2>Rendered Link:</h2>
+      <div className='mb3'>
+        <GatewayLink cidOrURL={cid} statusChecker={statusChecker}>{cid}</GatewayLink>
+      </div>
+      <h2>Rendered Image:</h2>
+      <div className='mb3'>
+        <GatewayImage cidOrURL={cid} statusChecker={statusChecker} alt={`Image of ${cid}`} className='mw6' />
+      </div>
     </div>
   )
 }
 
-function GatewayLink({ cid, title }) {
+function GatewayLink ({ cidOrURL, statusChecker, fallbackGatewayURL, children, ...props }) {
   const [url, setUrl] = useState('')
-  useEffect(async () => setUrl(await getGatewayURL(cid)), [cid])
-  return url ? <a href={url}>{title}</a> : null
+  useEffect(() => {
+    getGatewayURL(cidOrURL, { statusChecker, fallbackGatewayURL }).then(setUrl)
+  }, [cidOrURL, statusChecker, fallbackGatewayURL])
+  return url ? <a href={url} {...props}>{children}</a> : null
 }
 
-function GatewayImage({ cid, alt }) {
+function GatewayImage ({ cidOrURL, statusChecker, fallbackGatewayURL, alt, ...props }) {
   const [url, setUrl] = useState('')
-  useEffect(async () => setUrl(await getGatewayURL(cid)), [cid])
-  return url ? <img src={url} alt={alt} /> : null
+  useEffect(() => {
+    getGatewayURL(cidOrURL, { statusChecker, fallbackGatewayURL }).then(setUrl)
+  }, [cidOrURL, statusChecker, fallbackGatewayURL])
+  return url ? <img src={url} alt={alt} {...props} /> : null
 }
 
-export default App
+/**
+ * Custom status checker that simulates an adverse status from the nft.storage
+ * gateway status API.
+ */
+const fakeStatusChecker = {
+  ok: () => new Promise(resolve => resolve(false)),
+  gatewayURL: 'https://nftstorage.link'
+}
